f87f00699225934f31da0cff6f7018aacd39d8b7
Denis Dinkov <denisdinkov7@gmail.com>
2024-02-09 13:58:34 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import PageNav from "./components/PageNav";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <PageNav />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
-        <Route path="Pricing" element={<Pricing />} />
+        <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
