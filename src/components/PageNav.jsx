520ae4e01e96ac71acd58c892d71980a31473976
Denis Dinkov <denisdinkov7@gmail.com>
2024-02-09 17:41:23 +0200

    .

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom";
+import stlyes from "./PageNav.module.css";
+import Logo from "./Logo";
 
 export default function PageNav() {
   return (
-    <nav>
+    <nav className={stlyes.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
